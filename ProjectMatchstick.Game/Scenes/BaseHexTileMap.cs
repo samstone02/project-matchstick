489a379b525b7bbc013f5fa566f959e7df6f970d
Sam <samuelrystone@gmail.com>
2024-06-30 21:06:17 -0400

    slight project restructure

---
+++ ProjectMatchstick.Game/Scenes/BaseHexTileMap.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class BaseHexTileMap : TileMap
+{
+    [Export]
+    public Node Sample {  get; set; }
+}
---
+++ ProjectMatchstick.Services/Generation/SampleReader.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+using System.Collections.Generic;
+
+namespace ProjectMatchstick.Services.Generation;
+
+public static class SampleReader
+{
+    public static Dictionary<Vector2I, int> SampleFromTileMap(TileMap tileMap)
+    {
+        var sample = new Dictionary<Vector2I, int>();
+
+        foreach (var cell in tileMap.GetUsedCells(0))
+        {
+            sample.Add(cell, tileMap.GetCellTileData(0, cell).Terrain);
+        }
+
+        return sample;
+    }
+}
